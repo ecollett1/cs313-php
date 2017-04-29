33116fa99c510075a7f0102c9cecf4a4b55af668
ecollett1 <ericmichaelcollett@gmail.com>
2017-04-29 15:33:26 -0600

    Made Homepage and added links to future sites.

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>Coilett Industries
+		<meta charset="utf-8">
+		<title>Coilett Industries</title>
+	</head>
+	<body>
+		<h1>Home Page</h1>
+		<p>The Home Page!!</p>
+	</body>
+</html>
+
+<?php
+	require("nav.php");
+?>
---
+++ project/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Eric Collett's Assignments</title>
+    <style>
+      ul#menu li
+        {
+          text-align: center;
+        }
+      
+    </style>
+  </head>
+  <body>
+    <h1 style="text-align:center;">Eric Collett's 313 Assignments</h1>
+    
+    <ul id="menu" style="list-style-type:none">
+      <li><a href="assign1.php">CS 313 Work</a></li>
+      <br>
+      <li><a href="FullSite1.php">My Crazy Site</a></li>
+      <br>
+    </ul>
+    
+  </body>
+</html>
