5608268ad16f538f14b40b3dab0d083147b61fab
ecollett1 <ericmichaelcollett@gmail.com>
2017-05-17 15:59:57 -0600

    added the php files for all the links

--- web/project/callitphotography.php
+++ web/project/callitphotography.php
@@ -13,7 +13,7 @@
     <h1 id="large"><img src="Call it.PNG" alt="Title"/> </h1>
     <ul style="text-align: center;list-style-type: none;">
       <li id="links" style="list-style-type: none;text-align:center;display: inline;">
-        <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="index.php">Home</a></span>
+        <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="callitphotography.php">Home</a></span>
         <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="photos.php">Photos</a></span>
         <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="prices.php">Prices</a></span>
         <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="browse.php">CS 313 Work</a></span>
