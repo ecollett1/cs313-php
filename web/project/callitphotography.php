99aaaad82ce020e064c7a8d1c84810063a33fd4a
ecollett1 <ericmichaelcollett@gmail.com>
2017-05-20 09:23:57 -0600

    Moved header to another file

--- web/project/callitphotography.php
+++ web/project/callitphotography.php
@@ -9,18 +9,9 @@
   </head>
   <body id="mainContainer" class="fade-out">
     <main>
-
-    <h1 id="large"><img src="Call it.PNG" alt="Title"/> </h1>
-    <ul style="text-align: center;list-style-type: none;">
-      <li id="links" style="list-style-type: none;text-align:center;display: inline;">
-        <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="callitphotography.php">Home</a></span>
-        <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="photos.php">Photos</a></span>
-        <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="prices.php">Prices</a></span>
-        <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="browse.php">CS 313 Work</a></span>
-        <a style="font-size: 18px;color: black;text-decoration: none;" href="contact.html">Contact Me</a>
-      </li>
-    </ul>
-    <hr>
+    <?php
+      require("header.php");
+    ?>
     <br>
     <br>
     <div style="margin-left: auto;margin-right: auto;max-width: 500px;max-height: 600px">
---
+++ web/project/header.php
@@ -0,0 +1,11 @@
+<h1 id="large"><img src="Call it.PNG" alt="Title"/> </h1>
+    <ul style="text-align: center;list-style-type: none;">
+      <li id="links" style="list-style-type: none;text-align:center;display: inline;">
+        <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="callitphotography.php">Home</a></span>
+        <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="photos.php">Photos</a></span>
+        <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="prices.php">Prices</a></span>
+        <span style="padding-right: 30px;"><a style="font-size: 18px;color: black;text-decoration: none;" href="browse.php">CS 313 Work</a></span>
+        <a style="font-size: 18px;color: black;text-decoration: none;" href="contact.html">Contact Me</a>
+      </li>
+    </ul>
+    <hr>
