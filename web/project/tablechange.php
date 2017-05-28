bc36c9b77440f53a0ed29944540f7ce1f3757dde
ecollett1 <ericmichaelcollett@gmail.com>
2017-05-28 17:30:53 -0600

    Tried to fix my code for heroku...

--- web/project/tablechange.php
+++ web/project/tablechange.php
@@ -1,15 +1,20 @@
 <?php
-	session_start();
-	$dbURL = getenv('DATABASE_URL');
-    $dbopts = parse_url($dbURL);
+	$fname = htmlspecialchars($_POST['firstName']);
+	$lname = htmlspecialchars($_POST['lastName']);
+	$email = htmlspecialchars($_POST['email']);
+	$phone = htmlspecialchars($_POST['phone']);
+	$address = htmlspecialchars($_POST['address']);
 
-    $dbHost = $dbopts["host"];
-    $dbPort = $dbopts["port"];
-    $dbUser = $dbopts["user"];
-    $dbPassword = $dbopts["pass"];
-    $dbName = ltrim($dbopts["path"],'/');
+	try {
+		$dbURL = getenv('DATABASE_URL');
+	    $dbopts = parse_url($dbURL);
 
-    try {
+	    $dbHost = $dbopts["host"];
+	    $dbPort = $dbopts["port"];
+	    $dbUser = $dbopts["user"];
+	    $dbPassword = $dbopts["pass"];
+	    $dbName = ltrim($dbopts["path"],'/');
+
     	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
     } catch (PDOException $ex) {
     	echo "Error connecting to the db. Details: $ex";
@@ -17,39 +22,34 @@
     	die();
     }
 
-	if (!empty($_POST["firstName"])) {
-		$fname = htmlspecialchars($_POST['firstName']);
-		$query = "UPDATE customer SET firstname = :fname WHERE id = 1"
-		$statement = $db->prepare($query);
+	if (!empty($fname)) {
+		$query1 = "UPDATE customer SET firstname = :fname WHERE id = 1"
+		$statement = $db->prepare($query1);
     	$statement->bindValue(":fname", $fname, PDO::PARAM_STR);
     	$statement->execute();
 	}
 
-	if (!empty($_POST["lastName"])) {
-		$lname = htmlspecialchars($_POST['lastName']);
-		$query = "UPDATE customer SET lastname = :lname WHERE id = 1"
-		$statement = $db->prepare($query);
+	if (!empty($lname) {
+		$query2 = "UPDATE customer SET lastname = :lname WHERE id = 1"
+		$statement = $db->prepare($query2);
     	$statement->bindValue(":lname", $lname, PDO::PARAM_STR);
     	$statement->execute();
 	}
-	if (!empty($_POST["email"])) {
-		$email = htmlspecialchars($_POST['email']);
-		$query = "UPDATE customer SET email = :email WHERE id = 1"
-		$statement = $db->prepare($query);
+	if (!empty($email) {
+		$query3 = "UPDATE customer SET email = :email WHERE id = 1"
+		$statement = $db->prepare($query3);
     	$statement->bindValue(":email", $email, PDO::PARAM_STR);
     	$statement->execute();
 	}
-	if (!empty($_POST["phone"])) {
-		$phone = htmlspecialchars($_POST['phone']);
-		$query = "UPDATE customer SET phone = :phone WHERE id = 1"
-		$statement = $db->prepare($query);
+	if (!empty($phone) {
+		$query4 = "UPDATE customer SET phone = :phone WHERE id = 1"
+		$statement = $db->prepare($query4);
     	$statement->bindValue(":phone", $phone, PDO::PARAM_STR);
     	$statement->execute();
 	}
-	if (!empty($_POST["address"])) {
-		$address = htmlspecialchars($_POST['address']);
-		$query = "UPDATE customer SET address = :address WHERE id = 1"
-		$statement = $db->prepare($query);
+	if (!empty($address) {
+		$query5 = "UPDATE customer SET address = :address WHERE id = 1"
+		$statement = $db->prepare($query5);
     	$statement->bindValue(":address", $address, PDO::PARAM_STR);
     	$statement->execute();
 	}
