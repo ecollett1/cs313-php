ab8a9fc427b61b5d8b48c7f74dab2eba32edeed0
ecollett1 <ericmichaelcollett@gmail.com>
2017-06-02 14:54:27 -0600

    Tried to center images

--- web/project/photos.php
+++ web/project/photos.php
@@ -12,11 +12,13 @@
       <?php
         require("header.php");
       ?>
-      <table border="1" width="100%">
+      <table border="0" width="100%">
         <tr>
           <th class="center">Family<a href="photosfamily.php"><img id="gallery" src="13.jpg"></a></th>
           <th class="center">Engagements<a href="photosengage.php"><img id="gallery" src="7.JPG"></a></th>
           <th class="center">Wedding<a href="photoswedding.php"><img id="gallery" src="11.JPG"></a></th>
+        </tr>
+        <tr>
           <th class="center">Animals<a href="photosanimal.php"><img id="gallery" src="5.JPG"></a></th>
           <th class="center">Newborn<a href="photosnewborn.php"><img id="gallery" src="2.JPG"></a></th>
         </tr>
