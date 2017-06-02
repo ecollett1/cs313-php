492c9aba29dd31c940b69e98b29f8323764cfad8
ecollett1 <ericmichaelcollett@gmail.com>
2017-06-01 18:43:45 -0600

    Tried to fix tablechange so it worked right.

--- web/project/tablechange.php
+++ web/project/tablechange.php
@@ -25,35 +25,30 @@
 	if (!empty($fname)) {
 		$query1 = "UPDATE customer SET firstname = :fname WHERE id = 1";
 		$statement1 = $db->prepare($query1);
-    	$statement1->bindValue(":fname", $fname, PDO::PARAM_STR);
+    	$statement1->bindValue(':fname', $fname, PDO::PARAM_STR);
     	$statement1->execute();
 	}
 
-	if (!empty($lname) {
+	if (!empty($lname)) {
 		$query2 = "UPDATE customer SET lastname = :lname WHERE id = 1";
 		$statement2 = $db->prepare($query2);
     	$statement2->bindValue(":lname", $lname, PDO::PARAM_STR);
     	$statement2->execute();
 	}
-	if (!empty($email) {
-		$query3 = "UPDATE customer SET email = :email WHERE id = 1";
-		$statement3 = $db->prepare($query3);
-    	$statement3->bindValue(":email", $email, PDO::PARAM_STR);
-    	$statement3->execute();
-	}
-	if (!empty($phone) {
+
+	if (!empty($phone)) {
 		$query4 = "UPDATE customer SET phone = :phone WHERE id = 1";
 		$statement4 = $db->prepare($query4);
     	$statement4->bindValue(":phone", $phone, PDO::PARAM_STR);
     	$statement4->execute();
 	}
-	if (!empty($address) {
+	if (!empty($address)) {
 		$query5 = "UPDATE customer SET address = :address WHERE id = 1";
 		$statement5 = $db->prepare($query5);
     	$statement5->bindValue(":address", $address, PDO::PARAM_STR);
     	$statement5->execute();
 	}
 
-	header("contact.php");
+	header("Location: contact.php");
 	die();
 ?>
