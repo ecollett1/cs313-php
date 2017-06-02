cd2d88f9790c34a67fac6406661baba82ca2eca7
ecollett1 <ericmichaelcollett@gmail.com>
2017-06-02 14:44:38 -0600

    Tried to make the table span the whole screen

--- web/project/photos.php
+++ web/project/photos.php
@@ -21,11 +21,11 @@
           <th>Newborn</th>
         </tr>
         <tr>
-          <td id="gallery"><a href="photosfamily.php"><img id="gallery" src="13.jpg"></a></td>
-          <td id="gallery"><a href="photosengage.php"><img id="gallery" src="7.JPG"></a></td>
-          <td id="gallery"><a href="photoswedding.php"><img id="gallery" src="11.JPG"></a></td>
-          <td id="gallery"><a href="photosanimal.php"><img id="gallery" src="5.JPG"></a></td>
-          <td id="gallery"><a href="photosnewborn.php"><img id="gallery" src="2.JPG"></a></td>
+          <td id="gallery"><a href="photosfamily.php"><img id="gallery" class="center" src="13.jpg"></a></td>
+          <td id="gallery"><a href="photosengage.php"><img id="gallery" class="center" src="7.JPG"></a></td>
+          <td id="gallery"><a href="photoswedding.php"><img id="gallery" class="center" src="11.JPG"></a></td>
+          <td id="gallery"><a href="photosanimal.php"><img id="gallery" class="center" src="5.JPG"></a></td>
+          <td id="gallery"><a href="photosnewborn.php"><img id="gallery" class="center" src="2.JPG"></a></td>
         </tr>
       </table>
       <br>
