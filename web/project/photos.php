579f619fc3fdda3c87f45e8fb8ff7f4c05c0e66b
ecollett1 <ericmichaelcollett@gmail.com>
2017-06-02 14:46:41 -0600

    Tried to center images

--- web/project/photos.php
+++ web/project/photos.php
@@ -12,7 +12,7 @@
       <?php
         require("header.php");
       ?>
-      <table border="0" width="100%">
+      <table border="1" width="100%">
         <tr>
           <th>Family</th>
           <th>Engagements</th>
