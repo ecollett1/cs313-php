6538f5d6df712eb49ae8bdac8a00e15b06c38ab8
ecollett1 <ericmichaelcollett@gmail.com>
2017-06-14 23:48:14 -0600

    Added the way to send the email to Emily.

--- web/project/contact.php
+++ web/project/contact.php
@@ -15,18 +15,51 @@
       <br>
       <br>
       <h2> Contact Me </h2>
+      
+      <?php
+      /* PHP code to send email provided graciously by https://www.123contactform.com/html-contact-form/ 
+      All other PHP was made by the owner of this file (Eric Collett).*/
+        $action = $_REQUEST['action'];
+        
+        if ($action=="")    /* display the contact form */
+            {
+            ?>
+
       <form id="contactMessage" action="#" method="POST" enctype="multipart/form-data">
-        <label for="name">Name:</label><br />
-        <input id="name" class="input" name="name" type="text" value="" size="30">
-          <br>
-        <label for="email">Email:</label><br />
-        <input id="email" class="input" name="email" type="text" value="" size="30">
-          <br>
-        <label for="message">Message:</label><br />
-        <textarea id="message" class="input" name="message" rows="10" cols="40"></textarea>
-          <br>
-        <input id="submit_button" type="submit" value="Send email">
-      </form>    
+        <div class="center">
+          <label for="name">Name:</label><br />
+          <input id="name" class="input" name="name" type="text" value="" size="30">
+            <br>
+          <label for="email">Email:</label><br />
+          <input id="email" class="input" name="email" type="text" value="" size="30">
+            <br>
+          <label for="message">Message:</label><br />
+          <textarea id="message" class="input" name="message" rows="10" cols="40"></textarea>
+            <br>
+          <input id="submit_button" type="submit" value="Send email">
+        </div>
+      </form>
+
+      <?php
+            } 
+        else                /* send the submitted data */
+            {
+            $name = $_REQUEST['name'];
+            $email = $_REQUEST['email'];
+            $message = $_REQUEST['message'];
+            if ($name == "" || $email == "" || $message == "")
+                {
+                echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+                }
+            else{        
+                $from = "From: $name<$email>\r\nReturn-path: $email";
+                $subject = "Message sent using your contact form";
+                mail("callitphotography@gmail.com", $subject, $message, $from);
+                echo "Email sent!";
+                }
+            }  
+      ?>
+
       <?php
         try {
           $dbURL = getenv('DATABASE_URL');
