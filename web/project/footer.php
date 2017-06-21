8c1b02a015487b71545953e351e00609f1eb7e17
ecollett1 <ericmichaelcollett@gmail.com>
2017-06-21 17:02:28 -0600

    Added footer to the website.

---
+++ web/project/footer.php
@@ -0,0 +1,10 @@
+<ul>
+	<li>
+		<p>Creator: Eric Collett</p>
+		<p>Owner: Emily Collett</p>
+		<p>Contact Information:
+		<a href="mailto:someone@example.com">callitphotography@gmail.com</a>
+		<br>
+		<a href="tel:9494663580">(949)466-3580</a>
+	</li>
+</ul>
