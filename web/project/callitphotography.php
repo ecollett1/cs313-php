4333a33bc0f1beed0b91c93981068afe2d2a4f22
ecollett1 <ericmichaelcollett@gmail.com>
2017-06-24 20:50:11 -0600

    Added modal.

--- web/project/callitphotography.php
+++ web/project/callitphotography.php
@@ -5,7 +5,7 @@
 
     <title>Call It Photography</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-	<script rel="stylesheet" type="text/javascript" src="javascript.js"></script>
+
   </head>
   <body id="mainContainer" class="fade">
     <main>
@@ -27,6 +27,34 @@
     </div>
 
 </div>
+	<script>
+		// Get the modal
+		var modal = document.getElementById("myModal");
+
+		// Get the button that opens the modal
+		var btn = document.getElementById("myBtn");
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks on the button, open the modal 
+		btn.onclick = function () {
+			modal.style.display = "block";
+		}
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function () {
+			modal.style.display = "none";
+		}
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function (event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		}
+		</script>
+
     <div id="pictures">
       <div class="slides fade">
         <img id="images" src="1.JPG" alt="First Picture">
@@ -75,6 +103,7 @@
       ?>
     </div>
     </main>
+    <script rel="stylesheet" type="text/javascript" src="javascript.js" defer></script>
       
   </body>
 </html>
