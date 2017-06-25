aafa76558764b6fe0e5d6fb10cbd4e1b2c31cd32
ecollett1 <ericmichaelcollett@gmail.com>
2017-06-24 20:53:39 -0600

    Couldn't get a hover over box to show.

--- web/project/callitphotography.php
+++ web/project/callitphotography.php
@@ -14,47 +14,6 @@
     ?>
     <br>
     <br>
-	<button id="myBtn">Open Modal</button>
-
-	<div id="myModal" class="modal" style="display: none">
-	  <div class="modal-content">
-        <span class="close">&times;</span>
-        <input type="radio" name="choice" value="family">Family <br>
-		<input type="radio" name="choice" value="engagement">Engagements <br>
-		<input type="radio" name="choice" value="animal">Animal <br>
-		<input type="radio" name="choice" value="newborn">Newborn <br>
-		<input type="radio" name="choice" value="wedding">Wedding <br>
-    </div>
-
-</div>
-	<script>
-		// Get the modal
-		var modal = document.getElementById("myModal");
-
-		// Get the button that opens the modal
-		var btn = document.getElementById("myBtn");
-
-		// Get the <span> element that closes the modal
-		var span = document.getElementsByClassName("close")[0];
-
-		// When the user clicks on the button, open the modal 
-		btn.onclick = function () {
-			modal.style.display = "block";
-		}
-
-		// When the user clicks on <span> (x), close the modal
-		span.onclick = function () {
-			modal.style.display = "none";
-		}
-
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function (event) {
-			if (event.target == modal) {
-				modal.style.display = "none";
-			}
-		}
-		</script>
-
     <div id="pictures">
       <div class="slides fade">
         <img id="images" src="1.JPG" alt="First Picture">
