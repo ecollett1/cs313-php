a1ba2f26b98336c3115d60c0b17b137d245551d3
ecollett1 <ericmichaelcollett@gmail.com>
2017-07-13 23:41:47 -0600

    Email link was wrong.

--- web/project/footer.php
+++ web/project/footer.php
@@ -3,7 +3,7 @@
 		<p class="center">Owner: Emily Collett</p>
 		<p class="center">Creator: Eric Collett</p>
 		<p class="center">Contact Information:
-		<a href="mailto:someone@example.com">callitphotography@gmail.com</a>
+		<a href="mailto:callitphotography@gmail.com">callitphotography@gmail.com</a>
 		<br>
 		<a class="center" href="tel:5092050586">(509)205-0586</a>
 		</p>
