dbd56b051825956d3e5602c3c77c0459b83e04c8
Eric Michael Collett <ericmichaelcollett@ldschurch.org>
2020-01-08 11:56:33 -0700

    Added footer tag around footer items

--- web/project/footer.php
+++ web/project/footer.php
@@ -1,9 +1,11 @@
 <br>
+<footer>
 <hr>
-		<p class="center">Owner: Emily Collett</p>
-		<p class="center">Creator: Eric Collett</p>
-		<p class="center">Contact Information:
-		<a href="mailto:callitphotography@gmail.com">callitphotography@gmail.com</a>
-		<br>
-		<a class="center" href="tel:5092050586">(509)205-0586</a>
-		</p>
+<p class="center">Owner: Emily Collett</p>
+<p class="center">Creator: Eric Collett</p>
+<p class="center">Contact Information:
+<a href="mailto:callitphotography@gmail.com">callitphotography@gmail.com</a>
+<br>
+<a class="center" href="tel:5092050586">(509)205-0586</a>
+</p>
+</footer>
